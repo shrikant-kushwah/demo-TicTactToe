27d71a8f8463ac8803cfeb8309139d5e5178ca00
shrikant-kushwah <shrikant20052001@gmail.com>
2024-03-03 22:59:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TicTactToe from './components/TicTactToe/TicTactToe';
+
+const App = ()=> {
+  return (
+    <div className="App">
+      <TicTactToe/>
+    </div>
+  );
+}
+
+export default App;
